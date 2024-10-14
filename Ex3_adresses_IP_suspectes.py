49e73c87d34d4bfafb5c4cada8dc24328c7c179b
snirpy <76662720+snirpy@users.noreply.github.com>
2024-10-14 22:03:55 +0200

    Update and rename Ex3_table_multiplication.py to Ex3_adresses_IP_suspectes.py

---
+++ Ex3_adresses_IP_suspectes.py
@@ -0,0 +1,14 @@
+blacklist = ['192.168.1.5', '10.0.0.7', '172.16.0.10']
+ips_connected = ['192.168.1.1', '192.168.1.5', '192.168.1.10', '10.0.0.7']
+
+# 1. Détection des IP suspectes
+malicious_ips = [ip for ip in ips_connected if ip in blacklist]
+
+print("IP suspectes détectées :")
+for ip in malicious_ips:
+    print(ip)
+
+if malicious_ips:
+    print("\nAction suggérée : Bloquer ces IPs.")
+else:
+    print("\nAucune IP suspecte détectée.")
