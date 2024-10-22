3b7d55aa725d4a351541d7ff3441fc58df811191
snirpy <76662720+snirpy@users.noreply.github.com>
2024-10-22 18:32:23 +0200

    Update Ex3_adresses_IP_suspectes.py

--- Ex3_adresses_IP_suspectes.py
+++ Ex3_adresses_IP_suspectes.py
@@ -4,6 +4,12 @@ ips_connected = ['192.168.1.1', '192.168.1.5', '192.168.1.10', '10.0.0.7']
 # 1. Détection des IP suspectes
 malicious_ips = [ip for ip in ips_connected if ip in blacklist]
 
+## ou bien
+#malicious_ips = []
+#for ip in ips_connected:
+#    if ip in blacklist:
+#        malicious_ips.append(ip)
+
 print("IP suspectes détectées :")
 for ip in malicious_ips:
     print(ip)
