f08ba655ee2e72b5a4d731b40f5580a3f33044f8
snirpy <76662720+snirpy@users.noreply.github.com>
2024-10-22 18:26:15 +0200

    Update Ex2_analyse_ journaux_de_connexion.py

--- Ex2_analyse_ journaux_de_connexion.py
+++ Ex2_analyse_ journaux_de_connexion.py
@@ -1,44 +1,57 @@
-logs = [
-    ('192.168.1.2', '2024-09-12 12:00:10'),
-    ('192.168.1.2', '2024-09-12 12:00:10'),
-    ('192.168.1.12', '2024-09-12 12:35:30'),
-    ('192.168.1.1', '2024-09-12 13:00:15'),
-    ('192.168.1.1', '2024-09-12 13:16:15'),
-    ('192.168.1.12', '2024-09-12 13:35:30'),
-    ('192.168.1.12', '2024-09-12 14:00:30'),
-    ('192.168.1.1', '2024-09-12 15:16:15'),
-    ('192.168.1.3', '2024-09-12 16:16:15'),
-    ('192.168.1.112', '2024-09-12 17:35:30'),
-    ('192.168.1.120', '2024-09-12 17:35:30'),
-    ('192.168.1.192', '2024-09-12 17:35:30'),
-    ('192.168.1.222', '2024-09-12 17:35:30')
-]
+logs = [('192.168.1.50', '2024-09-12 12:15:10'), 
+        ('192.168.1.10', '2024-09-12 13:15:10'), 
+        ('192.168.1.20', '2024-09-12 14:16:15'), 
+        ('192.168.1.30', '2024-09-12 15:20:20'), 
+        ('192.168.1.10', '2024-09-12 16:20:20'),
+        ('192.168.1.30', '2024-09-12 17:20:20'),  
+        ('192.168.1.40', '2024-09-12 18:00:25'), 
+        ('192.168.1.20', '2024-09-12 18:16:15'), 
+        ('192.168.1.10', '2024-09-12 19:20:20'),
+        ('192.168.1.100', '2024-09-12 20:35:30') 
+        ] 
+ips= []
+horaires= []
+# print(len(logs))
+for ip, horaire in logs:
+    ips.append(ip)
+    horaires.append(horaire)
 
-liste_ip_unique =[]
-liste_ip_suspecte =[]
-occurence = []
-compteur_ip= 0
+# print(ips)
+# print(horaires)
 
-for element in logs:
-    if element[0] not in liste_ip_unique:
-        liste_ip_unique.append(element[0])
+
+ip_unique_liste = []
+ip_suspect_liste = []
+ip_suspect_unique_liste = []
+occurence=[]
+
+for log in logs:
+    if  log[0] not in ip_unique_liste:
+        ip_unique_liste.append(log[0])
+
     else:
-        compteur_ip += 1
-        liste_ip_suspecte.append(element[0])
-        
-for element in liste_ip_unique:
-    occurence.append(liste_ip_suspecte.count(element)+1) 
+        ip_suspect_liste.append(log[0])
 
-print(f"Le nombre d'adresses ip uniques est : {len(liste_ip_unique)}")
-print("Adresse suspecte: ")
-print(list(set(liste_ip_suspecte)))
-# print(compteur_ip)
-# print(occurence)
-print("Affichage:")
-for index, element in enumerate(liste_ip_unique):
-    # print(index)
-    if occurence[index]>=3:
-        print(f"{element} s'est connecté {occurence[index]} fois la première à {[c[1] for c in logs if c[0] == element][0]} ")
-        print(f"{element} s'est connecté {occurence[index]} fois la dernière à {[c[1] for c in logs if c[0] == element][-1]} ")
-    else:
-        print(f"{element} s'est connecté {occurence[index]} fois")
+for ip in ip_suspect_liste:
+    if ip not in ip_suspect_unique_liste:
+        ip_suspect_unique_liste.append(ip)
+
+for ip in ip_unique_liste:
+    occurence.append(ip_suspect_liste.count(ip)+1)
+
+# print(f"Les suspectes: {ip_suspect_unique_liste}")
+# print(f"Les bonnes: {ip_unique_liste}")
+# print(f"Les occurences: {occurence}")
+print("IP connectée 3 fois ou plus:\n")
+for ip in ip_suspect_unique_liste:
+    # print("index:",index)
+    # print("ip:",ip)
+    # if ip in ip_suspect_unique_liste:
+    indices_de_ip = [i for i, x in enumerate(ips) if x == ip]
+    # print(indices_de_ip)
+    # print(f"{ip} : occurence : {ips.count(ip)} {horaires[ips.index(ip)]}")
+    # print(f"{ip} : occurence : {ips.count(ip)} {horaires[indices_de_ip[-1]]}")
+    print(f"{ip} : première connexion {horaires[ips.index(ip)]}")
+    print(f"{ip} : dernière connexion {horaires[indices_de_ip[-1]]}")
+    print()
+print("Nombre total de connexion unique: ",len(ip_unique_liste))
